1fd12a24383d928909524998bbad44e557fb531d
Louponius <59686499+Louponius@users.noreply.github.com>
2021-10-20 13:16:58 +0200

    Let's hope that this is the last time I edit this
    I didn't want to edit the actual files since they are supposed to be preserved.
    Also, I am not sorry for the terrible sitemap.
    
    -- Oklomsy

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
   echo "Sparksammy.herokuapp.com";
   echo "<br>ilikema.ga hosted for the world to see:";
   echo "<br>Explore the wonders: <br><a href='index.html'>index.html (A broken Js redirection page)</a><br><a href='lel.html'>lel.html (Wall of ads)</a><br><a href='main.html'>main.html (GO HERE)</a>";
-  echo "<br><h2>If you want to visit the real ilikema.ga go to these sites:";
+  echo "<br><h2>If you want to visit the real ilikema.ga go to these sites:</h2>";
   echo "<br><a href='TVI/index.html'>TVI hate letter</a><br><a href='homepage/index.html'>homepage/index.html (A working version of Ilikema.ga)</a><br><a href='oklomsy_site/index.html'>oklomsy_site/index.html</a><br><a href='oklomsy_site/index.html.save'>oklomsy_site/index.html (old version)</a><br><a href='oklomsy_site/oklomsy.html'>oklomsy_site/oklomsy.html</a><br><a href='oklomsy_site/oklomsyads.html'>oklomsy_site/oklomsyads.html (Oklomsy's wall of ads)</a><br><a href='privoplayer/index.html'>privoplayer/index.html (A broken media player)</a><hr><p>Have a good day!</p>";
 ?>
