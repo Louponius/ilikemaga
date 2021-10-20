65eaf2c219fcaeabbfac2c491720b2b321ee994b
Louponius <59686499+Louponius@users.noreply.github.com>
2021-10-20 13:06:35 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
-  echo "Let's keep Heroku happy with a index.php :)";
+  echo "Sparksammy.herokuapp.com";
+  echo "ilikema.ga hosted for the world to see:";
+  echo "Explore the wonders: <a href='index.html'>index.html (A broken Js redirection page)</a><a href='lel.html'>lel.html (Wall of ads)</a><a href='main.html'>main.html (GO HERE)</a>"; 
 ?>
